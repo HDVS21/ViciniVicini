442729d73a6ad95689cd074b4c863a1f7df24541
Simon NAUD <162438554+nomisduan@users.noreply.github.com>
2025-03-03 16:34:30 +0100

    BlurredMiniGame created

---
+++ ArtApp/ArtApp/BlurredMiniGameView.swift
@@ -0,0 +1,18 @@
+//
+//  BlurredMiniGameView.swift
+//  ArtApp
+//
+//  Created by Simon Naud on 03/03/25.
+//
+
+import SwiftUI
+
+struct BlurredMiniGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlurredMiniGameView()
+}
