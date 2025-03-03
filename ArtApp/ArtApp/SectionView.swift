3e73a83b76352f1d448c33e6e37ba9fa8f132985
Simon NAUD <162438554+nomisduan@users.noreply.github.com>
2025-03-03 17:47:49 +0100

    Sections title added to SectionView

--- ArtApp/ArtApp/SectionView.swift
+++ ArtApp/ArtApp/SectionView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct SectionView: View {
     var body: some View {
-        ScrollView {
-            VStack {
-                SectionTileView(sectionTitle: "Classicism", sectionCategory: "Movement", sectionImage: "classicism", isUnlocked: true, userProgress: 0.7)
-                SectionTileView(sectionTitle: "Leonardo da Vinci", sectionCategory: "Artist", sectionImage: "stJohnTheBaptist", isUnlocked: false, userProgress: 0.0);  SectionTileView(sectionTitle: "Classicism", sectionCategory: "Movement", sectionImage: "classicism", isUnlocked: false, userProgress: 0.0);
-                
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 15) {
+                    SectionTileView(sectionTitle: "Classicism", sectionCategory: "Movement", sectionImage: "classicism", isUnlocked: true, userProgress: 0.7)
+                    SectionTileView(sectionTitle: "Leonardo da Vinci", sectionCategory: "Artist", sectionImage: "stJohnTheBaptist", isUnlocked: false, userProgress: 0.0);
+                    SectionTileView(sectionTitle: "Classicism", sectionCategory: "Movement", sectionImage: "classicism", isUnlocked: false, userProgress: 0.0);
+                    SectionTileView(sectionTitle: "Leonardo da Vinci", sectionCategory: "Artist", sectionImage: "stJohnTheBaptist", isUnlocked: false, userProgress: 0.0);
+                    SectionTileView(sectionTitle: "Classicism", sectionCategory: "Movement", sectionImage: "classicism", isUnlocked: false, userProgress: 0.0);
+                    
+                }
+                .padding()
             }
-            .padding()
+            .navigationTitle("Sections")
+            
         }
         
     }
