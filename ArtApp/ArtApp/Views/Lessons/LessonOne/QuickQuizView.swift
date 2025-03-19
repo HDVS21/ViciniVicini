a2b59db1858c84cc3de4bef57eae463898d166ad
Simon NAUD <162438554+nomisduan@users.noreply.github.com>
2025-03-19 15:11:13 +0100

    AlertMessage implemented to QuickQuiz

--- ArtApp/ArtApp/Views/Lessons/LessonOne/QuickQuizView.swift
+++ ArtApp/ArtApp/Views/Lessons/LessonOne/QuickQuizView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Simon Naud on 11/03/25.
 //
-
 import SwiftUI
 
 struct QuickQuizView: View {
@@ -18,117 +17,111 @@ struct QuickQuizView: View {
     
     @State private var currentQuestionIndex = 0
     @State private var selectedAnswer: String?
-    @State private var isAnswerCorrect: Bool?
+    @State private var isAnswerCorrect: Bool = false
     @State private var showAlert = false
-    @State private var alertMessage = ""
     @State private var showNextButton = false
     @State private var navigateToLesson = false
     
     var body: some View {
-        VStack(alignment: .center) {
-            QuizHeader(title: "Quick Quiz", lettersColor: "pink-letters", backgroundColor: "pink-background", path: $path)
-            
-            let currentQuestion = quizDetails[currentQuestionIndex]
-            
-            VStack(alignment: .center, spacing: 10) {
-                Spacer()
-                Text(currentQuestion.question)
-                    .multilineTextAlignment(.center)
-                    .frame(height: 50)
+        ZStack {
+            VStack(alignment: .center) {
+                QuizHeader(title: "Quick Quiz", lettersColor: "pink-letters", backgroundColor: "pink-background", path: $path)
                 
-                Spacer()
+                let currentQuestion = quizDetails[currentQuestionIndex]
                 
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .foregroundColor(Color("pink-options"))
-                        .frame(width: 280, height: 159)
-                    Image(currentQuestion.painting)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 280, height: 159)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                }
-                .clipped()
-                Spacer()
-                
-                
-                ForEach(currentQuestion.answers, id: \.self) { answer in
-                    Button(action: {
-                        selectedAnswer = answer
-                    }) {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 16)
-                                .foregroundColor(selectedAnswer == answer ? Color("pink-background") : Color("pink-options"))
-                                .frame(width: 280, height: 53)
-                            Text(answer)
-                                .foregroundStyle(.black)
+                VStack(alignment: .center, spacing: 10) {
+                    Spacer()
+                    Text(currentQuestion.question)
+                        .multilineTextAlignment(.center)
+                        .frame(height: 50)
+                    Spacer()
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(Color("pink-options"))
+                            .frame(width: 280, height: 159)
+                        Image(currentQuestion.painting)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 280, height: 159)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                    }
+                    .clipped()
+                    Spacer()
+                    
+                    ForEach(currentQuestion.answers, id: \ .self) { answer in
+                        Button(action: {
+                            selectedAnswer = answer
+                        }) {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 16)
+                                    .foregroundColor(selectedAnswer == answer ? Color("pink-background") : Color("pink-options"))
+                                    .frame(width: 280, height: 53)
+                                Text(answer)
+                                    .foregroundStyle(.black)
+                            }
+                        }
+                    }
+                    Spacer()
+                    
+                    if !showNextButton {
+                        Button(action: {
+                            guard let selectedAnswer = selectedAnswer else { return }
+                            isAnswerCorrect = selectedAnswer == currentQuestion.correctAnswer
+                            showAlert = true
+                            
+                            if isAnswerCorrect {
+                                showNextButton = true
+                            }
+                        }) {
+                            ZStack {
+                                Capsule()
+                                    .frame(width: 215, height: 42)
+                                    .foregroundStyle(selectedAnswer != nil ? Color("pink-buttons") : Color.gray)
+                                    .shadow(radius: 3)
+                                Text("Confirm Selection")
+                                    .foregroundStyle(.white)
+                                    .fontWeight(.semibold)
+                            }
+                        }
+                    }
+                    
+                    if showNextButton {
+                        Button(action: {
+                            if currentQuestionIndex < quizDetails.count - 1 {
+                                currentQuestionIndex += 1
+                                selectedAnswer = nil
+                                isAnswerCorrect = false
+                                showAlert = false
+                                showNextButton = false
+                            } else {
+                                path.removeLast(2)
+                                navigateToLesson = true
+                            }
+                        }) {
+                            ZStack {
+                                Capsule()
+                                    .frame(width: 215, height: 42)
+                                    .foregroundStyle(Color("pink-buttons"))
+                                    .shadow(radius: 3)
+                                Text(currentQuestionIndex < quizDetails.count - 1 ? "Next Question" : "Finish Quiz")
+                                    .foregroundStyle(.white)
+                                    .fontWeight(.semibold)
+                            }
                         }
                     }
                 }
-                Spacer()
-                
-                if !showNextButton {
-                    Button(action: {
-                        guard let selectedAnswer = selectedAnswer else { return }
-                        isAnswerCorrect = selectedAnswer == currentQuestion.correctAnswer
-                        print(selectedAnswer)
-                        print(currentQuestion.correctAnswer)
-                        alertMessage = isAnswerCorrect! ? "Correct!" : "Incorrect! Try again."
-                        showAlert = true
-                        
-                        if isAnswerCorrect! {
-                            showNextButton = true
-                        }
-                    }) {
-                        ZStack {
-                            Capsule()
-                                .frame(width: 215, height: 42)
-                                .foregroundStyle(selectedAnswer != nil ? Color("pink-buttons") : Color.gray)
-                                .shadow(radius: 3)
-                            Text("Confirm Selection")
-                                .foregroundStyle(.white)
-                                .fontWeight(.semibold)
-                        }
-                    }
-                }
-                
-                if showNextButton {
-                    Button(action: {
-                        if currentQuestionIndex < quizDetails.count - 1 {
-                            currentQuestionIndex += 1
-                            selectedAnswer = nil
-                            isAnswerCorrect = nil
-                            showAlert = false
-                            showNextButton = false
-                        } else {
-                            // Remove the last two views from NavigationPath when finishing the quiz
-                            path.removeLast(2)
-                            navigateToLesson = true
-                        }
-                    }) {
-                        ZStack {
-                            Capsule()
-                                .frame(width: 215, height: 42)
-                                .foregroundStyle(Color("pink-buttons"))
-                                .shadow(radius: 3)
-                            Text(currentQuestionIndex < quizDetails.count - 1 ? "Next Question" : "Finish Quiz")
-                                .foregroundStyle(.white)
-                                .fontWeight(.semibold)
-                        }
-                    }
-                }
+                .padding()
             }
-            .padding()
-            .alert(isPresented: $showAlert) {
-                Alert(
-                    title: Text(isAnswerCorrect! ? "Correct!" : "Incorrect!"),
-                    message: Text(alertMessage),
-                    dismissButton: .default(Text("OK"))
-                )
+            .navigationBarHidden(true)
+            .edgesIgnoringSafeArea(.top)
+            
+            if showAlert {
+                AlertMessage(color: "pink", isCorrect: $isAnswerCorrect, showAlert: $showAlert, message: ("", ""))
+                    .frame(width: 260, height: 140)
+                    .transition(.scale)
             }
         }
-        .navigationBarHidden(true)
-        .edgesIgnoringSafeArea(.top)
     }
 }
 
